d3e291f78cfa21bd28d3b9c696f81fb75ffe398f
Ethan Pineda <joshuapineda66@gmail.com>
2024-02-28 10:12:22 -0600

    adjusting content of prof experiences and ecs, also changed style of bullet points on the timelines

--- src/app/_components/Extracurriculars/Extracurriculars.tsx
+++ src/app/_components/Extracurriculars/Extracurriculars.tsx
@@ -8,7 +8,7 @@ interface ExtracurricularObject {
     position: string;
     duration: string;
     link: string; 
-    description: Array<string>;
+    description: string; // Changed from Array<string> to string
 };
 
 export default function Extracurriculars() {
@@ -19,42 +19,28 @@ export default function Extracurriculars() {
             position: "Webmaster",
             duration: "September 2022 - Present", 
             link: "https://emerging-coders-website.vercel.app/", 
-            description: [
-                "Developed and maintained the organization's website using Next.js, Tailwind CSS, and Vercel",
-                "Directed technical workshop series and mentored more than 50 students in full-stack development, cloud computing, and data structures and algorithms",
-                "Collaborated with other Computer Science organizations and company sponsors to host joint events and workshops revolving around technical development and career readiness"
-            ],
+            description: "As the Webmaster, I developed and maintained the site with Next.js and Tailwind CSS, led technical workshops, mentored over 50 students, and collaborated on events with sponsors.",
         },
         {
             organization: "The Society of Hispanic Professional Engineers (SHPE)",
             position: "MentorSHPE Chair", 
             duration: "March 2022 - Present", 
             link: "https://www.instagram.com/shpe_nu/", 
-            description: [
-                "Directed mentorship program for 75 first-generation and low-income Latinx engineering/STEM majors",
-                "Increased overall participation in program by 30% through more emphasis on recruitment of first-year students",
-                "Organize event panel for graduating seniors to share their experiences at Northwestern for an audience of 40 people"
-            ],
+            description: "Directed the mentorship program for 75 Latinx engineering majors, boosting participation by 30% and organizing senior panels to enhance career readiness.",
         },
         {
             organization: "ColorStack at Northwestern", 
             position: "Founder and Treasurer",
             duration: "May 2023 - Present",
             link: "https://www.instagram.com/colorstacknu/", 
-            description: [
-                "Successfully secured over $3,000 in funding from various organizations and companies.",
-                "Coordinated and managed the organization's finances, with a focus on obtaining corporate sponsorships to support our initiatives",
-            ],
+            description: "As Founder and Treasurer, secured over $3,000 in funding and managed finances with a focus on corporate sponsorships to support our initiatives.",
         },
         {
             organization: "Alianza - The Latinx Student Alliance",
             position: "Historian", 
             duration: "March 2022 - March 2023", 
             link: "https://www.instagram.com/nu_alianza/",
-            description: [
-                "Supervise recording of flyers, social media post, and advertisements of events and meetings associated with Alianza",
-                "Maintain an account of Latinx history at Northwestern and its broader community",
-            ],
+            description: "Recorded and maintained historical accounts of Alianza's events and Latinx history at Northwestern, involving extensive documentation and social media engagement.",
         },
     ];
     
@@ -68,17 +54,15 @@ export default function Extracurriculars() {
                     <ol className="relative border-s border-paragraph-gray dark:border-paragraph-gray">
                         {myExtracurriculars.map((activity, index) => (
                             <li key={index} className="mb-10 ml-4 w-md">
-                                <div className="absolute w-3 h-3 bg-paragraph-gray rounded-full mt-1.5 -start-1.5 border border-orange dark:orange dark:bg-orange"></div>
+                                <div className="absolute w-3 h-3 bg-paragraph-gray rounded mt-1.5 -start-1.5 border border-orange dark:orange dark:bg-orange"></div>
                                 <div className="w-full sm:w-full md:w-5/6 lg:w-5/6 xl:w-5/6">
                                     <time className="mb-1 text-sm font-normal leading-none text-paragraph-gray dark:text-paragraph-gray">{activity.duration}</time>
                                     <a href={activity.link} target="_blank"><h3 className="text-2xl font-bold text-orange">{activity.organization}</h3></a>
                                     <h4 className="mb-1 text-lg font-medium text-paragraph-gray dark:text-paragraph-gray">{activity.position}</h4>
-                                    {activity.description.map((desc, descIndex) => (
-                                        <div key={descIndex} className="mb-4">
-                                            <span className="text-orange mr-2">▹</span>
-                                            <span className="text-base font-normal text-paragraph-gray dark:text-paragraph-gray">{desc}</span>
-                                        </div>
-                                    ))}
+                                    <div className="mb-4">
+                                        <span className="text-orange mr-2">▹</span>
+                                        <span className="text-base font-normal text-paragraph-gray dark:text-paragraph-gray">{activity.description}</span>
+                                    </div>
                                 </div>
                             </li>
                         ))}
--- src/app/_components/ProfessionalExperience/ProfessionalExperience.tsx
+++ src/app/_components/ProfessionalExperience/ProfessionalExperience.tsx
@@ -2,13 +2,13 @@
 
 import SectionHeader from "../ui/SectionHeader";
 import FadeInSection from "../FadeInSection/FadeInSection";
-import exp from "constants";
+
 interface ProfessionalExperienceObject {
     company: string;
     duration: string;
     title: string;
     link: string; 
-    description: Array<string>;
+    description: string; // Changed from Array<string> to string
 };
 
 export default function ProfessionalExperience() {
@@ -19,71 +19,48 @@ export default function ProfessionalExperience() {
             duration: "June 2023 - August 2023", 
             title: "Full Stack Software Engineer Intern",
             link: "https://www.salesforce.com/products/data/", 
-            description: [
-                "Selected into a cohort of 60 out of 2000 applicants into Salesforce FTL Program, an intensive 10-week internship focusing on full stack development including: front-end, back-end, testing, design, and deployment",
-                "Led, planed, and developed within. A team of 3 a 4-week MVP in an Agile setting using React.js, Node.js, Express.js, and PostgresSQL to create Telemedicine applications for immigrants to the United States", 
-                "Performed and led daily standups and weekly sprints, wrote and assigned user stories in backlog which allowed team to accomplish MVP and include stretch features in final product"
-            ],
+            description: "Selected for Salesforce's FTL Program, led a team to develop a Telemedicine application using React.js and Node.js, and conducted Agile sprints and standups.",
         },
         {
             company: "Ka moamoa Lab", 
             duration: "September 2023 - Present",
             title: "Undergraduate Research Assistant",
             link: "https://kamoamoa.com/", 
-            description: [
-                "Collaborated with PhD candidate Stefany Cruz on innovative safety-based wearable technology projects utilizing Machine Learning Sound Classification and Edge Impulse for real-time environmental threat detection",
-                "Spearheaded the development of a mobile application using Flutter to interface with wearable device, enhancing user accessibility to safety alerts for dangerous sounds.", 
-                "Engineered a solution to offload computational processing between the wearable technology and mobile application, optimizing battery life and device efficiency", 
-            ],
+            description: "Collaborated on safety-based wearable technology using machine learning for threat detection, developed a Flutter app for user alerts, and optimized processing for device efficiency.",
         }, 
         {
             company: "Technological Innovations for Inclusive Learning and Teaching (tiilt)",
             duration: "March 2022 - Present",
             title: "Undergraduate Research Assistant",
             link: "https://tiilt.northwestern.edu/", 
-            description: [
-                "Partnered with the BLINC project to enhance and implement interactive and accessible front-end features to improve user experience for the real-time multimodal learning analytics platform with 50 daily users",
-                "Developed a new data structure for the Racket programming language, which is a functional programming language",
-                "Implemented the data structure in Racket and wrote a paper on the data structure"
-            ],
+            description: "Worked with the BLINC project to improve a learning analytics platform, developed a new data structure in Racket, and contributed to a related paper.",
         },
         {
             company: "Northwestern University",
             duration: "June 2022 - Present", 
             title: "Undergraduate Teaching Assistant", 
             link: "https://www.mccormick.northwestern.edu/computer-science/", 
-            description: [
-                "Dedicated 6-10 hours weekly for office hours to aid more than 800 students in Data Structures and Algorithms", 
-                "Worked closely with Professor Vincent St. Amour, Sruti Bhagavatula, and Leif Rasmussen to give feedback and grades on student code and examinations", 
-                "Awarded Outstanding Peer Mentor Award in recognition of high performance and dedication to student learning"
-            ],
+            description: "Supported 800+ students in Data Structures and Algorithms, contributed to grading and feedback, and received the Outstanding Peer Mentor Award.",
         },
         {
             company: "CodePath", 
             duration: "September 2023 - Present", 
             title: "Web Development Tech Fellow",
             link: "https://www.codepath.org/", 
-            description: [
-                "Delivered in-depth technical support for 70+ students focusing on sophisticated web development concepts, including the construction of web servers, organization of web data, and management of RESTful API interactions.",
-                "Ensured prompt resolution of technical issues via Slack, assisting students with challenges in dynamic and relational data handling, mastering GitHub workflows, implementing authentication protocols, and adopting deployment methodologies.", 
-                "Cultivated a nurturing educational atmosphere, building rapport with learners and facilitating their ongoing progress and mastery of course materials."
-            ],
+            description: "Provided technical support for 70+ students in web development, facilitated issue resolution, and fostered a supportive educational environment.",
         }, 
         {
             company: "Ruta Health", 
             duration: "January 2023 - June 2023",
             title: "Student Software Engineering",
             link: "https://www.ycombinator.com/companies/ruta-health", 
-            description: [
-               "Worked with healthcare startup and actively contribute to the development and launch of beta product, including leading the development of a React frontend with a Node.js backend resulting in improved performance and scalability" 
-            ],
+            description: "Contributed to a healthcare startup by leading frontend development with React and Node.js, enhancing product performance and scalability.",
         }, 
     ];
 
-
     return (
         <>
-            <div className="flex flex-col items-center justify-center min-h-screen text-paragraph-gray px-4 sm:px-6 lg:px-8 max-w-screen-xl mx-auto" mb-10>
+            <div className="flex flex-col items-center justify-center min-h-screen text-paragraph-gray px-4 sm:px-6 lg:px-8 max-w-screen-xl mx-auto mb-10">
                 <FadeInSection delay={100}>
                     <div className="text-left w-full">
                         <SectionHeader title="where-i-work" />
@@ -91,17 +68,15 @@ export default function ProfessionalExperience() {
                         <ol className="relative border-s border-paragraph-gray dark:border-paragraph-gray">
                             {myProfessionalExperience.map((experience, index) => (
                                 <li key={index} className="mb-10 ml-4 w-md">
-                                    <div className="absolute w-3 h-3 bg-paragraph-gray rounded-full mt-1.5 -start-1.5 border border-orange dark:border-orange dark:bg-orange"></div>
+                                    <div className="absolute w-3 h-3 bg-paragraph-gray rounded mt-1.5 -start-1.5 border border-orange dark:border-orange dark:bg-orange"></div>
                                     <div className="w-full sm:w-full md:w-5/6 lg:w-5/6 xl:w-5/6">
                                         <time className="mb-1 text-md font-normal text-sm leading-none text-paragraph-gray dark:text-paragraph-gray">{experience.duration}</time>
                                         <a href={experience.link} target="_blank"><h3 className="text-2xl font-bold text-orange">{experience.company}</h3></a>
                                         <h4 className="mb-1 text-lg font-medium text-paragraph-gray dark:text-paragraph-gray">{experience.title}</h4>
-                                        {experience.description.map((desc, descIndex) => (
-                                            <div key={descIndex} className="mb-4">
-                                                <span className="text-orange mr-2">▹</span>
-                                                <span className="text-base font-normal text-paragraph-gray">{desc}</span>
-                                            </div>
-                                        ))}
+                                        <div className="mb-4">
+                                            <span className="text-orange mr-2">▹</span>
+                                            <span className="text-base font-normal text-paragraph-gray">{experience.description}</span>
+                                        </div>
                                     </div>
                                 </li>
                             ))}
