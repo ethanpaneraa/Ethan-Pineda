023330b6db165a4067e7bd3199df46fedba05b80
Ethan Pineda <joshuapineda66@gmail.com>
2024-02-28 01:57:06 -0600

    progress for today

--- src/app/_components/About/About.tsx
+++ src/app/_components/About/About.tsx
@@ -20,17 +20,17 @@ export default function About() {
                 <div className="mt-5 w-full flex flex-col md:flex-row justify-center items-center md:items-start text-md">
                     <div className="w-full mb-6 md:mb-0 md:pr-4 lg:pr-8">
                         <p className="mb-4">I am currently a third-year at
-                            <a href="https://www.northwestern.edu/about/" target="_blank" rel="noopener noreferrer" className="text-orange no-underline">
+                            <a href="https://www.northwestern.edu/about/" target="_blank" className="text-orange font-semibold">
                                 {" "}Northwestern University
                             </a>
                             {" "}where I am pursuing my Bachelors of Arts in {""}
-                            <strong className="text-orange">Computer Science</strong> with minors in <strong className="text-orange">Data Science</strong> and <strong className="text-orange">Latino/Latina Studies</strong>.
+                            <strong className="text-orange">Computer Science</strong> with minors in <strong className="text-orange font-semibold">Data Science</strong> and <strong className="text-orange font-semibold">Latino/Latina Studies</strong>.
                         </p>
                         <p className="mb-4">
-                            After my studies, I hope to work as a <strong className="text-orange">Full-Stack Software Engineer</strong> with a focus on building scalable, performant, and user-friendly applications.
+                            After my studies, I hope to work as a <strong className="text-orange font-semibold">Full-Stack Software Engineer</strong> with a focus on building scalable, performant, and user-friendly applications.
                         </p>
                         <p className="mb-4">
-                            At Northwestern, I&apos;ve had the privilege of working with various research groups and professors, I also serve on the executive board of multiple different student organizations that are aimed at enhancing diversity and inclusion in tech, and interned at amazing companies, including <a className="text-orange">Salesforce</a>, where I was a Software Engineer Intern and will be returning to for the summer of 2024!
+                            At Northwestern, I&apos;ve had the privilege of working with various research groups and professors, I also serve on the executive board of multiple different student organizations that are aimed at enhancing diversity and inclusion in tech, and interned at amazing companies, including <a className="text-orange font-semibold">Salesforce</a>, where I was a Software Engineer Intern and will be returning to for the summer of 2024!
                         </p>
                         <div className="text-left mb-5">
                             <p>Here are a few technologies I have been working with recently:</p>
