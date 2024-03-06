ea519943f993cf722f94086172f49720c2ad6e89
Ethan Pineda <joshuapineda66@gmail.com>
2024-03-06 02:21:44 -0600

    done with all core components. TODO: add blog

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -4,6 +4,14 @@ export default function Footer() {
 
     return (
         <>
+            <footer className="rounded-lg shadow m-4">
+                <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                    <hr className="my-6 border-paragraph-gray sm:mx-auto lg:my-8" />
+                    <span className="block text-sm text-paragraph-gray sm:text-center">
+                        &copy; 2024 Built and Designed by Ethan Pineda. All rights reserved.
+                    </span>
+                </div>
+            </footer>
         </>
     );
 };
