f2c004388c3e6334c79e261eae4890465c04bf95
Ethan Pineda <joshuapineda66@gmail.com>
2025-07-01 00:33:18 -0500

    remove placeholder code

--- src/lib/generate-placeholder.ts
+++
@@ -1,6 +0,0 @@
-import { getPlaiceholder } from "plaiceholder";
-
-export async function generatePlaceholder(buffer: Buffer): Promise<string> {
-  const { base64 } = await getPlaiceholder(buffer);
-  return base64;
-}
