d6e52ce993adf957fde19f6b0352ad773c671d07
Ethan Pineda <joshuapineda66@gmail.com>
2025-07-07 17:08:58 -0500

    small typo fix

--- src/components/about/about-content.tsx
+++ src/components/about/about-content.tsx
@@ -16,11 +16,11 @@ export default function AboutContent() {
 
         <div className="space-y-4">
           <p className="leading-relaxed animate-fade-in-up">
-            i'm a first-generation mexican-american software engineer, born and
-            raised in the west side of chicago. i am the first in my family to
-            pursue a career in technology as a software engineer and many of the
-            things that i have done in my life have been too pave the way for
-            others like me.
+            i&apos; a first-generation mexican-american software engineer, born
+            and raised in the west side of chicago. i am the first in my family
+            to pursue a career in technology as a software engineer and many of
+            the things that i have done in my life have been too pave the way
+            for others like me.
             <br />
             <br />
             because of so, during my time as an undergraduate, i had the
